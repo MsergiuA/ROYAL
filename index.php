44d4e4cfaec2cb76b7c1ffeefc26e1848148ebe3
sergiu <moldovan_alexandru_sergiu@yahoo.com>
2018-08-15 17:39:38 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,207 @@
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="resources/css/custom.css" type="text/css"/>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div>
+                <div class="row">
+                    <div class="header col-md-12 col-xs-12 tel-email-container">
+                        <p>T 86-21-153163733076 | E <a href="#">Contact@RoyalTec.com.cn</a></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="navbar col-md-12 col-xs-12 nav-bar-ul" style="text-align: center;">
+                        <div class="col-md-6 offset-1">
+                            <nav class="nav d-flex justify-content-between">
+                                <a class="p-2 col-md-2 menuItem" href="#">Royal</a>
+                                <a class="p-2 col-md-2 menuItem" href="#">Products</a>
+                                <a class="p-2 col-md-2 menuItem" href="#">Application</a>
+                                <a class="p-2 col-md-2 menuItem" href="#">Services</a>
+                                <a class="p-2 col-md-2 menuItem" href="#">Article</a>
+                                <a class="p-2 col-md-2 menuItem" href="#">Contact</a>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="header col-md-12 col-xs-12 extra-menu-div" style="display: none">
+                        <div class="col-md-10 offset-1">
+                            <div class="row body-of-hidden-div">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 carrousel-no-padding-class">
+                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="resources/images/sew.jpeg" alt="First slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="resources/images/second.jpeg" alt="Second slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="resources/images/third.jpeg" alt="Third slide">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10 offset-1 index-text">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h2>SERVICES</h2>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            </div>
+                            <div class="col-md-6">
+                                <h2>SERVICES SERVICES SERVICES</h2>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row little-more-info-index">
+                    <div class="col-md-10 offset-1 index-text">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <p>În ciuda opiniei publice, Lorem Ipsum nu e un simplu text fără sens. El îşi are rădăcinile într-o bucată a literaturii clasice latine din anul 45 î.e.n., făcând-o să aibă mai bine de 2000 ani. Profesorul universitar de latină de la colegiul Hampden-Sydney din Virginia, Richard McClintock, a căutat în bibliografie unul din cele mai rar folosite cuvinte latine "consectetur", întâlnit în pasajul Lorem Ipsum, şi căutând citate ale cuvântului respectiv în literatura clasică, a descoperit la modul cel mai sigur sursa provenienţei textului. Lorem Ipsum provine din secţiunile 1.10.32 şi 1.10.33 din "de Finibus Bonorum et Malorum" (Extremele Binelui şi ale Răului) de Cicerone, scrisă în anul 45 î.e.n. Această carte este un tratat în teoria eticii care a fost foarte popular în perioada Renasterii. Primul rând din Lorem Ipsum, "Lorem ipsum dolor sit amet...", a fost luat dintr-un rând din secţiunea 1.10.32.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row our-products-index-section">
+                    <div class="col-md-10 offset-1 index-text">
+                        <div class="row">
+                            <div class="col-md-12" style="text-align: center; font-size: 40px; color: orange; padding-top: 0px">
+                                Our Products
+                            </div>
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-6 our-products-index-div-box">
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                                    </div>
+                                    <div class="col-md-6 our-products-index-div-box">
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                                    </div>
+                                    <div class="col-md-6 our-products-index-div-box">
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                                    </div>
+                                    <div class="col-md-6 our-products-index-div-box">
+                                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row footer-section">
+                <div class="col-md-10 offset-1 index-text">
+                    <div class="row">
+                        <div class="col-md-12" style="text-align: center; font-size: 40px; color: orange;">
+                            <h2>PWD VACUUM Coating Machine & Magnetron Sputtering Coating Machine</h2>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <nav class="nav d-flex justify-content-between">
+                                        <a href="#">Royal</a>
+                                        <a href="#">Royal</a>
+                                        <a href="#">Royal</a>
+                                        <a href="#">Royal</a>
+                                    </nav>
+                                </div>
+                                <div class="col-md-6" style="text-align: center;">
+                                    <p>T 86-21-153163733076 | E <a href="#">Contact@RoyalTec.com.cn</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+
+    <script>
+        var products = [
+            'Masina',
+            'Casa',
+            'Bani',
+            'Muraturi'
+        ];
+
+        var royal = [
+            'Cola',
+            'Fanta',
+            'Sprite',
+            'Cacat'
+        ];
+
+        $('.menuItem').hover(function(){
+            $('.body-of-hidden-div').empty();
+            $('.extra-menu-div').css('display','inline');
+
+            var linkContent = $(this).text();
+            linkContent  = linkContent.toLowerCase();
+            console.log(linkContent);
+
+            if(linkContent == 'royal') {
+                $(royal).each(function (i) {
+                    var content = '<a class="p-2 col-md-2 hidden-element-text" href="#">' + royal[i] + '<hr class="custom-line-for-menu-items"></a>';
+                    $('.body-of-hidden-div').append(content);
+                });
+            }else if(linkContent == 'products'){
+                $(products).each(function (i) {
+                    var content = '<a class="p-2 col-md-2 hidden-element-text" href="#">' + products[i] + '<hr class="custom-line-for-menu-items"></a>';
+                    $('.body-of-hidden-div').append(content);
+                });
+            }else{
+                $('.body-of-hidden-div').empty();
+                $('.extra-menu-div').css('display','none');
+            }
+        });
+
+        $('.extra-menu-div').mouseleave(function(){
+            $('.body-of-hidden-div').empty();
+            $('.extra-menu-div').css('display','none');
+        });
+
+        // $('.menuItem').mouseleave(function(){
+        //     $('.body-of-hidden-div').empty();
+        //     $('.extra-menu-div').css('display','none');
+        // });
+    </script>
+    </body>
+</html>
