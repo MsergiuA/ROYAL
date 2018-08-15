5e8053f3a67e8856b75edfdbf393f0cddee51be6
sergiu <moldovan_alexandru_sergiu@yahoo.com>
2018-08-15 17:56:14 +0300

    Split code to multiple files and added includes to index

---
+++ footer.php
@@ -0,0 +1,24 @@
+<div class="row footer-section">
+    <div class="col-md-10 offset-1 index-text">
+        <div class="row">
+            <div class="col-md-12" style="text-align: center; font-size: 40px; color: orange;">
+                <h2>PWD VACUUM Coating Machine & Magnetron Sputtering Coating Machine</h2>
+            </div>
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-6">
+                        <nav class="nav d-flex justify-content-between">
+                            <a href="#">Royal</a>
+                            <a href="#">Royal</a>
+                            <a href="#">Royal</a>
+                            <a href="#">Royal</a>
+                        </nav>
+                    </div>
+                    <div class="col-md-6" style="text-align: center;">
+                        <p>T 86-21-153163733076 | E <a href="#">Contact@RoyalTec.com.cn</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ header-section.php
@@ -0,0 +1,29 @@
+<div class="menu-and-header-section">
+    <div class="row">
+        <div class="header col-md-12 col-xs-12 tel-email-container">
+            <p>T 86-21-153163733076 | E <a href="#">Contact@RoyalTec.com.cn</a></p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="navbar col-md-12 col-xs-12 nav-bar-ul" style="text-align: center;">
+            <div class="col-md-6 offset-1">
+                <nav class="nav d-flex justify-content-between">
+                    <a class="p-2 col-md-2 menuItem" href="#">Royal</a>
+                    <a class="p-2 col-md-2 menuItem" href="#">Products</a>
+                    <a class="p-2 col-md-2 menuItem" href="#">Application</a>
+                    <a class="p-2 col-md-2 menuItem" href="#">Services</a>
+                    <a class="p-2 col-md-2 menuItem" href="#">Article</a>
+                    <a class="p-2 col-md-2 menuItem" href="#">Contact</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="header col-md-12 col-xs-12 extra-menu-div" style="display: none">
+            <div class="col-md-10 offset-1">
+                <div class="row body-of-hidden-div">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
